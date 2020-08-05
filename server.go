533f222c77fa273b56209193d934a52ffe0720b6
Shi Diyuan <shidiyuan@meituan.com>
2020-08-05 17:35:01 +0800

    small changes

--- server.go
+++ server.go
@@ -1,36 +1,30 @@
 package main
 
 import (
-	"log"
-	"middleware/handler"
-	"middleware/pgsql"
-	"net/http"
-	"os"
-	"path/filepath"
-	"golang.org/x/crypto/acme/autocert"
+        "log"
+        "middleware/handler"
+        "middleware/pgsql"
+        "net/http"
+        "golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatalf("resolve file abs path: %v", err)
-	}
 
-	db, err := pgsql.New(&pgsql.PGConfig{User: "alncl", Pass: "l;n1223", DBName: "blog"})
-	if err != nil {
-		log.Fatalf("setup db server: %v\n", err)
-	}
-	defer db.Close()
+        db, err := pgsql.New(&pgsql.PGConfig{User: "blogdbu", Pass: "123s;,nl", DBName: "blog"})
+        if err != nil {
+                log.Fatalf("setup db server: %v\n", err)
+        }
+        defer db.Close()
 
-	// srvConfig := &SrvConfig{Host: "172.31.41.201", Port: 8443}
-	srv := http.Server{
-		Addr:    ":8443",
-		Handler: handler.New(db),
-	}
+        // srvConfig := &SrvConfig{Host: "172.31.41.201", Port: 8443}
+        srv := http.Server{
+                Addr:    ":443",
+                Handler: handler.New(db),
+        }
 
-	crt := autocert.NewListener("api.redhand.vip")
+        crt := autocert.NewListener("api.redhand.vip")
 
-	if err := srv.Listen(crt); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("setup server: %v\n", err)
-	}
+        if err := srv.Serve(crt); err != nil && err != http.ErrServerClosed {
+                log.Fatalf("setup server: %v\n", err)
+        }
 }
